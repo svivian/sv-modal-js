e8ca0374c80586947472e5a4f85d7b5196214281
Scott <scott.vivian@gmail.com>
2021-01-21 15:57:08 +0000

    Restructure to allow multiple instances

--- sv-modal.js
+++ sv-modal.js
@@ -3,126 +3,135 @@ SV = window.SV || {};
 
 SV.Modal = (function() {
 
-	// private members
+	// constructor
+	return function(modalId) {
 
-	var modal = null;
-	var titleElem = null;
-	var contentElem = null;
-	var eventThrottle = false;
+		// private members
 
-	// private methods
+		var modal;
+		var titleElem;
+		var contentElem;
+		var eventThrottle = false;
 
-	var Constructor = function(modalId) {
-		// modal inner HTML
-		var modalHtml =
-			'<div class="modal">' +
-				'<span class="modal-close" title="Close">&times;</span>' +
-				'<h3 class="modal-title"></h3>' +
-				'<div class="modal-content"></div>' +
-			'</div>';
+		// private methods
 
-		modal = document.createElement('div');
-		modal.id = modalId;
-		modal.classList.add('modal-wrapper');
-		modal.innerHTML = modalHtml;
-		document.querySelector('body').appendChild(modal);
+		var init = function(modalId) {
+			// modal inner HTML
+			var modalHtml =
+				'<div class="modal">' +
+					'<span class="modal-close" title="Close">&times;</span>' +
+					'<h3 class="modal-title"></h3>' +
+					'<div class="modal-content"></div>' +
+				'</div>';
 
-		titleElem = modal.querySelector('.modal-title');
-		contentElem = modal.querySelector('.modal-content');
+			modal = document.createElement('div');
+			modal.id = modalId;
+			modal.classList.add('modal-wrapper');
+			modal.innerHTML = modalHtml;
+			document.querySelector('body').appendChild(modal);
 
-		// remove content and trigger event after fade out
-		modal.addEventListener('transitionend', function(ev) {
-			if (modal.classList.contains('visible'))
+			titleElem = modal.querySelector('.modal-title');
+			contentElem = modal.querySelector('.modal-content');
+
+			// remove content and trigger event after fade out
+			modal.addEventListener('transitionend', function(ev) {
+				if (modal.classList.contains('visible'))
+					return;
+
+				if (titleElem)
+					titleElem.innerHTML = '';
+				if (contentElem)
+					contentElem.innerHTML = '';
+
+				var closeEvent = new CustomEvent('sv.modal.close');
+				modal.dispatchEvent(closeEvent);
+			});
+
+			// trigger event on resize
+			contentElem.addEventListener('transitionend', function(ev) {
+				var validProps = ['width', 'height'];
+				if (eventThrottle || !validProps.includes(ev.propertyName))
+					return;
+
+				// prevent both width and height triggering event
+				eventThrottle = true;
+				modal.dispatchEvent(new CustomEvent('sv.modal.resize'));
+
+				// set a timeout to throttle
+				setTimeout(function() {
+					eventThrottle = false;
+				}, 200);
+			});
+
+			// handle clicks on close button and background
+			document.addEventListener('click', function(ev) {
+				var classes = ev.target.classList;
+				if (classes.contains('modal-wrapper') || classes.contains('modal-close'))
+					methods.close();
+			});
+		};
+
+
+		// public methods
+
+		var methods = {};
+
+		methods.getModalElement = function() {
+			return modal;
+		};
+
+		methods.getContentElement = function() {
+			return contentElem;
+		};
+
+		methods.getTitleElement = function() {
+			return titleElem;
+		};
+
+		methods.inject = function (content, title) {
+			if (!modal)
 				return;
 
 			if (titleElem)
-				titleElem.innerHTML = '';
+				titleElem.innerHTML = title;
 			if (contentElem)
-				contentElem.innerHTML = '';
+				contentElem.innerHTML = content;
+		};
 
-			var closeEvent = new CustomEvent('sv.modal.close');
-			modal.dispatchEvent(closeEvent);
-		});
-
-		// trigger event on resize
-		contentElem.addEventListener('transitionend', function(ev) {
-			var validProps = ['width', 'height'];
-			if (eventThrottle || !validProps.includes(ev.propertyName))
+		methods.show = function () {
+			if (!modal)
 				return;
 
-			// prevent both width and height triggering event
-			eventThrottle = true;
-			modal.dispatchEvent(new CustomEvent('sv.modal.resize'));
+			modal.classList.add('visible');
+		};
 
-			// set a timeout to throttle
-			setTimeout(function() {
-				eventThrottle = false;
-			}, 200);
-		});
+		methods.close = function () {
+			if (!modal)
+				return;
 
-		// handle clicks on close button and background
-		var ctor = this;
-		document.addEventListener('click', function(ev) {
-			var classes = ev.target.classList;
-			if (classes.contains('modal-wrapper') || classes.contains('modal-close'))
-				ctor.close();
-		});
+			modal.classList.remove('visible');
+			// fix loading if modal is closed then reopened quickly
+			eventThrottle = false;
+		};
+
+		methods.resizeContent = function (width, height) {
+			if (!modal)
+				return;
+
+			// explicitly set current size of modal, so that CSS transitions work
+			contentElem.style.width = contentElem.offsetWidth + 'px';
+			contentElem.style.height = contentElem.offsetHeight + 'px';
+
+			if (width && height) {
+				contentElem.style.width = width + 'px';
+				contentElem.style.height = height + 'px';
+			}
+		};
+
+
+		init(modalId);
+
+		return methods;
 	};
-
-	// public api
-
-	Constructor.prototype.getModalElement = function() {
-		return modal;
-	};
-
-	Constructor.prototype.getContentElement = function() {
-		return contentElem;
-	};
-
-	Constructor.prototype.getTitleElement = function() {
-		return titleElem;
-	};
-
-	Constructor.prototype.inject = function (content, title) {
-		if (!modal)
-			return;
-
-		if (titleElem)
-			titleElem.innerHTML = title;
-		if (contentElem)
-			contentElem.innerHTML = content;
-	};
-
-	Constructor.prototype.show = function () {
-		if (!modal)
-			return;
-
-		modal.classList.add('visible');
-	};
-
-	Constructor.prototype.close = function () {
-		if (!modal)
-			return;
-
-		modal.classList.remove('visible');
-		// fix loading if modal is closed then reopened quickly
-		eventThrottle = false;
-	};
-
-	Constructor.prototype.resizeContent = function (width, height) {
-		if (!modal)
-			return;
-
-		// explicitly set current size of modal, so that CSS transitions work
-		contentElem.style.width = contentElem.offsetWidth + 'px';
-		contentElem.style.height = contentElem.offsetHeight + 'px';
-
-		if (width && height) {
-			contentElem.style.width = width + 'px';
-			contentElem.style.height = height + 'px';
-		}
-	};
-
-	return Constructor;
 
 })();
